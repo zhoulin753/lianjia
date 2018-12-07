86fb907636d864e458097610e7eb1697784b9c12
zhoulin753 <1281263636@qq.com>
2018-12-07 18:46:04 +0800

    2

--- house/admin.py
+++ house/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from .models import House
 # Register your models here.
-@admin.register(House)
+# @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'houseinfo', 'area', 'finish', 'house_type',
                     'floor', 'address', 'cost', 'price', 'url']
     list_per_page = 30
 
-    search_fields = ('cost')
+    search_fields = ('price',)
 admin.site.register(House,HouseAdmin)
--- house/views.py
+++ house/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+
 from house.models import House
 # Create your views here.
 def index(request):
-    shuju = House.objects.get(pk=1011)
-    print(shuju.name, shuju.price, shuju.url)
-    return HttpResponse('hello world')
+    shuju = House.objects.all()
+    # print(shuju.name, shuju.price, shuju.url)
+    context = {'shuju':shuju}
+    temp = loader.get_template('index.html')
+    return render(request, 'index.html' ,context=context)
+    # return HttpResponse(temp.render(context))
