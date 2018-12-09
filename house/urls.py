572b4fd677d69fb1ced4db4af9ce04f33b340205
zhoulin753 <1281263636@qq.com>
2018-12-09 22:40:32 +0800

    2

--- house/urls.py
+++ house/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
 from house import views
 urlpatterns = [
-    url(r'^index/',views.index)
-
+    url(r'^index/',views.index),
+    url(r'pg(\d+)', views.get_page)
 
 
 ]
--- house/views.py
+++ house/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import RequestContext, loader
-
-
 from house.models import House
-# Create your views here.
+
+
+def info_context(shuju):
+    context = {}
+    num = House.objects.count()
+    page = num / 30
+    if page > int(page):
+        page = int(page) + 1
+    else:
+        page = int(page)
+    context['shuju'] = shuju
+    context['page'] = page
+    return context
+
+
 def index(request):
-    shuju = House.objects.all()
-    # print(shuju.name, shuju.price, shuju.url)
-    context = {'shuju':shuju}
-    temp = loader.get_template('index.html')
-    return render(request, 'index.html' ,context=context)
-    # return HttpResponse(temp.render(context))
+    shuju = House.objects.filter(pk__lte=30)
+    context = info_context(shuju)
+    return render(request, 'index.html', context=context)
+
+
+def get_page(request, page):
+    house_id = int(page) * 30 - 29
+    shuju = House.objects.filter(pk__lte=int(page) * 30, pk__gte=house_id)
+    context = info_context(shuju)
+    return render(request, 'index.html', context=context)
+
